fc1d3d2fdcbcaa587b44921969682c5f29afd73f
fangruijiao <fangruijiao@huaxiapawn.com>
2019-08-07 11:04:11 +0800

    验货页面优化

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -35,7 +35,93 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data() {
+    return {
+      testData: {
+        'a': 'aaa',
+        'b': 'bbb',
+        'c': 'ccc'
+      }
+    }
+  },
+  created() {
+    // const result = this.add(2,3,6,7)
+    // const result = this.Fibonacci2(1000)
+    // console.log(result, 'result')
+    // this.testIterator()
+    this.testArray()
+  },
+  methods: {
+
+    testArray() {
+      for (let data in this.testData) {
+        console.log(data, 'data111')
+      }
+      for (let data of this.testData) {
+        console.log(data, 'data111')
+      }
+    },
+
+    testIterator() {
+      // Number.prototype[Symbol.iterator] = function*() {
+      //   let i = 0;
+      //   let num = this.valueOf();
+      //   while (i < num) {
+      //     yield i++;
+      //   }
+      // }
+      // console.log([...5],'testIterator')
+      // let map = new Map([
+      //   [1, 'one'],
+      //   [2, 'two'],
+      //   [3, 'three'],
+      // ]);
+      // let arr = [...map.keys()];
+      // console.log(arr,'arr')
+      let arrayLike = {
+        '0': 'a',
+        '1': 'b',
+        '2': 'c',
+        length: 4
+      };
+      // ES5的写法
+      var arr1 = [].slice.call(arrayLike);
+      // ES6的写法
+      let arr2 = Array.from(arrayLike)
+      console.log(arr1,'arr1')
+      console.log(arr2,'arr2')
+    },
+    textAsset() {
+      const x = 10
+      const arr = [
+        ...(x > 0 ? ['a'] : []),
+        'b',
+      ]
+      console.log(arr, 'arr')
+    },
+    Fibonacci (n) {
+      if ( n <= 1 ) {return 1}
+      return this.Fibonacci(n - 1) + this.Fibonacci(n - 2);
+    },
+    Fibonacci2 (n, f1=1, f2=1) {
+      if ( n <= 1 ) {return f2}
+      return this.Fibonacci2(n - 1, f2, f1 + f2);
+    },
+    add (...reset) {
+      let sum = 0
+      for (var item of reset) {
+        sum += item
+      }
+      return sum
+    },
+    paramsErrorMsg () {
+      throw new Error("参数不正确！！")
+    },
+    test (id = this.paramsErrorMsg()) {
+      return id
+    }
+  },
 }
 </script>
 
